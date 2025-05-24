0404965095ac78d88f2b1521db545286ac290825
Mitzi Morris <mitzi212@gmail.com>
2025-05-24 14:53:08 -0400

    unit tests

--- src/test/unit/load_samplers_test.cpp
+++ src/test/unit/load_samplers_test.cpp
@@ -1,18 +1,12 @@
 #include <stan3/load_samplers.hpp>
-#include <stan3/load_model.hpp>
-#include <stan3/read_json_data.hpp>
 #include <stan3/hmc_nuts_arguments.hpp>
-#include <stan3/hmc_output_writers.hpp>
-#include <stan/callbacks/stream_logger.hpp>
-#include <stan/io/empty_var_context.hpp>
+#include <stan3/read_json_data.hpp>
 
 #include <test/test-models/bernoulli.hpp>
 
-#include <filesystem>
-#include <fstream>
+#include <stan/callbacks/stream_logger.hpp>
+
 #include <memory>
-#include <sstream>
-#include <variant>
 #include <vector>
 
 #include <gtest/gtest.h>
@@ -20,360 +14,143 @@
 class LoadSamplersTest : public ::testing::Test {
 protected:
   void SetUp() override {
-    test_dir = std::filesystem::temp_directory_path() / "stan3_load_samplers_test";
-    std::filesystem::create_directories(test_dir);
+    // Load test data and create model
+    auto data_context = stan3::read_json_data("src/test/test-models/bernoulli.data.json");
+    model_ = std::make_unique<bernoulli_model_namespace::bernoulli_model>(*data_context, 12345);
     
-    init_file = test_dir / "init.json";
-    std::ofstream init_stream(init_file);
-    init_stream << R"({ "theta": 0.5 })";
-    init_stream.close();
+    // Setup basic arguments
+    args_.num_chains = 2;
+    args_.random_seed = 12345;
+    args_.metric_type = stan3::metric_t::DIAG_E;
+    args_.stepsize = 1.0;
+    args_.max_depth = 10;
+    args_.delta = 0.8;
     
-    diag_metric_file = test_dir / "diag_metric.json";
-    std::ofstream diag_stream(diag_metric_file);
-    diag_stream << R"({ "inv_metric": [1.5] })";
-    diag_stream.close();
+    // Create empty init contexts (use default initialization)
+    for (size_t i = 0; i < args_.num_chains; ++i) {
+      init_contexts_.push_back(stan3::read_json_data(""));
+      metric_contexts_.push_back(stan3::read_json_data(""));
+      init_writers_.push_back(nullptr);
+    }
     
-    dense_metric_file = test_dir / "dense_metric.json";
-    std::ofstream dense_stream(dense_metric_file);
-    dense_stream << R"({ "inv_metric": [[2.0]] })";
-    dense_stream.close();
-    
-    logger = std::make_unique<stan::callbacks::stream_logger>(
+    logger_ = std::make_unique<stan::callbacks::stream_logger>(
       std::cout, std::cout, std::cout, std::cerr, std::cerr);
-    
-    // Load model and data
-    data_context = stan3::read_json_data("src/test/test-models/bernoulli.data.json");
-    stan::io::var_context* raw_context = const_cast<stan::io::var_context*>(data_context.get());
-    model = stan3::load_model(*raw_context, 12345);
   }
   
-  void TearDown() override {
-    std::error_code ec;
-    std::filesystem::remove_all(test_dir, ec);
-  }
-  
-  /* Helper to create init contexts */
-  std::vector<std::shared_ptr<const stan::io::var_context>>
-  create_init_contexts(size_t num_chains, bool use_file = true) {
-    std::vector<std::shared_ptr<const stan::io::var_context>> contexts;
-    contexts.reserve(num_chains);
-    for (size_t i = 0; i < num_chains; ++i) {
-      if (use_file) {
-        contexts.push_back(stan3::read_json_data(init_file.string()));
-      } else {
-        contexts.push_back(std::make_shared<stan::io::empty_var_context>());
-      }
-    }
-    return contexts;
-  }
-  
-  /* Helper to create metric contexts */
-  std::vector<std::shared_ptr<const stan::io::var_context>>
-  create_metric_contexts(size_t num_chains, stan3::metric_t metric_type) {
-    std::vector<std::shared_ptr<const stan::io::var_context>> contexts;
-    contexts.reserve(num_chains);
-    
-    for (size_t i = 0; i < num_chains; ++i) {
-      switch (metric_type) {
-        case stan3::metric_t::UNIT_E:
-          contexts.push_back(std::make_shared<stan::io::empty_var_context>());
-          break;
-        case stan3::metric_t::DIAG_E:
-          contexts.push_back(stan3::read_json_data(diag_metric_file.string()));
-          break;
-        case stan3::metric_t::DENSE_E:
-          contexts.push_back(stan3::read_json_data(dense_metric_file.string()));
-          break;
-      }
-    }
-    return contexts;
-  }
-  
-  /* Helper to create init writers */
-  std::vector<stan::callbacks::writer*> create_init_writers(size_t num_chains) {
-    dummy_writers.clear();
-    dummy_writers.resize(num_chains);
-    
-    std::vector<stan::callbacks::writer*> writers;
-    writers.reserve(num_chains);
-    for (size_t i = 0; i < num_chains; ++i) {
-      writers.push_back(&dummy_writers[i]);
-    }
-    return writers;
-  }
-  
-  std::filesystem::path test_dir;
-  std::filesystem::path init_file;
-  std::filesystem::path diag_metric_file;
-  std::filesystem::path dense_metric_file;
-  
-  std::unique_ptr<stan::callbacks::logger> logger;
-  std::shared_ptr<const stan::io::var_context> data_context;
-  std::unique_ptr<stan::model::model_base> model;
-  stan3::hmc_nuts_args args;
-  
-  // Storage for dummy writers
-  std::vector<stan::callbacks::writer> dummy_writers;
+  std::unique_ptr<bernoulli_model_namespace::bernoulli_model> model_;
+  stan3::hmc_nuts_args args_;
+  std::vector<std::shared_ptr<const stan::io::var_context>> init_contexts_;
+  std::vector<std::shared_ptr<const stan::io::var_context>> metric_contexts_;
+  std::vector<stan::callbacks::writer*> init_writers_;
+  std::unique_ptr<stan::callbacks::stream_logger> logger_;
 };
 
-TEST_F(LoadSamplersTest, LoadSamplersUnitE) {
-  args.metric_type = stan3::metric_t::UNIT_E;
-  args.num_chains = 1;
+TEST_F(LoadSamplersTest, CreateSamplers_DiagE) {
+  args_.metric_type = stan3::metric_t::DIAG_E;
   
-  auto init_contexts = create_init_contexts(args.num_chains);
-  auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-  auto init_writers = create_init_writers(args.num_chains);
+  auto sampler_configs = stan3::create_samplers(*model_, args_, init_contexts_, 
+                                               metric_contexts_, *logger_, init_writers_);
   
+  // Should return a variant - we can't easily check the exact type, 
+  // but we can verify it doesn't throw
   EXPECT_NO_THROW({
-    auto config = stan3::load_samplers<stan3::metric_t::UNIT_E>(
-      *model, args, init_contexts, metric_contexts, *logger, init_writers);
-    
-    EXPECT_EQ(config.samplers.size(), 1);
-    EXPECT_EQ(config.rngs.size(), 1);
-    EXPECT_EQ(config.init_params.size(), 1);
-    EXPECT_FALSE(config.init_params[0].empty());
+    std::visit([this](auto& config) {
+      EXPECT_EQ(config.samplers.size(), args_.num_chains);
+      EXPECT_EQ(config.rngs.size(), args_.num_chains);
+      EXPECT_EQ(config.init_params.size(), args_.num_chains);
+    }, sampler_configs);
   });
 }
 
-TEST_F(LoadSamplersTest, LoadSamplersDiagE) {
-  args.metric_type = stan3::metric_t::DIAG_E;
-  args.num_chains = 1;
-  
-  auto init_contexts = create_init_contexts(args.num_chains);
-  auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-  auto init_writers = create_init_writers(args.num_chains);
+TEST_F(LoadSamplersTest, CreateSamplers_UnitE) {
+  args_.metric_type = stan3::metric_t::UNIT_E;
   
   EXPECT_NO_THROW({
-    auto config = stan3::load_samplers<stan3::metric_t::DIAG_E>(
-      *model, args, init_contexts, metric_contexts, *logger, init_writers);
+    auto sampler_configs = stan3::create_samplers(*model_, args_, init_contexts_, 
+                                                 metric_contexts_, *logger_, init_writers_);
     
-    EXPECT_EQ(config.samplers.size(), 1);
-    EXPECT_EQ(config.rngs.size(), 1);
-    EXPECT_EQ(config.init_params.size(), 1);
-    EXPECT_FALSE(config.init_params[0].empty());
+    std::visit([this](auto& config) {
+      EXPECT_EQ(config.samplers.size(), args_.num_chains);
+      EXPECT_EQ(config.rngs.size(), args_.num_chains);
+      EXPECT_EQ(config.init_params.size(), args_.num_chains);
+    }, sampler_configs);
   });
 }
 
-TEST_F(LoadSamplersTest, LoadSamplersDenseE) {
-  args.metric_type = stan3::metric_t::DENSE_E;
-  args.num_chains = 1;
-  
-  auto init_contexts = create_init_contexts(args.num_chains);
-  auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-  auto init_writers = create_init_writers(args.num_chains);
+TEST_F(LoadSamplersTest, CreateSamplers_DenseE) {
+  args_.metric_type = stan3::metric_t::DENSE_E;
   
   EXPECT_NO_THROW({
-    auto config = stan3::load_samplers<stan3::metric_t::DENSE_E>(
-      *model, args, init_contexts, metric_contexts, *logger, init_writers);
+    auto sampler_configs = stan3::create_samplers(*model_, args_, init_contexts_, 
+                                                 metric_contexts_, *logger_, init_writers_);
     
-    EXPECT_EQ(config.samplers.size(), 1);
-    EXPECT_EQ(config.rngs.size(), 1);
-    EXPECT_EQ(config.init_params.size(), 1);
-    EXPECT_FALSE(config.init_params[0].empty());
+    std::visit([this](auto& config) {
+      EXPECT_EQ(config.samplers.size(), args_.num_chains);
+      EXPECT_EQ(config.rngs.size(), args_.num_chains);
+      EXPECT_EQ(config.init_params.size(), args_.num_chains);
+    }, sampler_configs);
   });
 }
 
-TEST_F(LoadSamplersTest, LoadSamplersMultipleChains) {
-  args.metric_type = stan3::metric_t::DIAG_E;
-  args.num_chains = 3;
+TEST_F(LoadSamplersTest, CreateSamplers_InvalidMetricType) {
+  args_.metric_type = static_cast<stan3::metric_t>(999);  // Invalid metric type
   
-  auto init_contexts = create_init_contexts(args.num_chains);
-  auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-  auto init_writers = create_init_writers(args.num_chains);
-  
-  EXPECT_NO_THROW({
-    auto config = stan3::load_samplers<stan3::metric_t::DIAG_E>(
-      *model, args, init_contexts, metric_contexts, *logger, init_writers);
-    
-    EXPECT_EQ(config.samplers.size(), 3);
-    EXPECT_EQ(config.rngs.size(), 3);
-    EXPECT_EQ(config.init_params.size(), 3);
-    
-    for (size_t i = 0; i < 3; ++i) {
-      EXPECT_FALSE(config.init_params[i].empty());
-    }
-  });
+  EXPECT_THROW({
+    stan3::create_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                          *logger_, init_writers_);
+  }, std::runtime_error);
 }
 
-// TEST_F(LoadSamplersTest, CreateSamplersVariantUnitE) {
-//   args.metric_type = stan3::metric_t::UNIT_E;
-//   args.num_chains = 1;
+TEST_F(LoadSamplersTest, LoadSamplers_DiagE_CorrectSizes) {
+  auto config = stan3::load_samplers<stan3::metric_t::DIAG_E>(
+    *model_, args_, init_contexts_, metric_contexts_, *logger_, init_writers_);
   
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
+  EXPECT_EQ(config.samplers.size(), args_.num_chains);
+  EXPECT_EQ(config.rngs.size(), args_.num_chains);
+  EXPECT_EQ(config.init_params.size(), args_.num_chains);
   
-//   EXPECT_NO_THROW({
-//     auto variant = stan3::create_samplers(*model, args, init_contexts, 
-//                                         metric_contexts, *logger, init_writers);
-    
-//     // Should hold unit_e config
-//     EXPECT_TRUE(std::holds_alternative<
-//       stan3::sampler_config<stan::mcmc::adapt_unit_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant));
-    
-//     auto& config = std::get<
-//       stan3::sampler_config<stan::mcmc::adapt_unit_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-//   });
-// }
+  // Check that init_params have the right size for each chain
+  for (size_t i = 0; i < args_.num_chains; ++i) {
+    EXPECT_EQ(config.init_params[i].size(), model_->num_params_r());
+  }
+}
 
-// TEST_F(LoadSamplersTest, CreateSamplersVariantDiagE) {
-//   args.metric_type = stan3::metric_t::DIAG_E;
-//   args.num_chains = 1;
+TEST_F(LoadSamplersTest, LoadSamplers_UnitE_CorrectSizes) {
+  auto config = stan3::load_samplers<stan3::metric_t::UNIT_E>(
+    *model_, args_, init_contexts_, metric_contexts_, *logger_, init_writers_);
   
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
+  EXPECT_EQ(config.samplers.size(), args_.num_chains);
+  EXPECT_EQ(config.rngs.size(), args_.num_chains);
+  EXPECT_EQ(config.init_params.size(), args_.num_chains);
   
-//   EXPECT_NO_THROW({
-//     auto variant = stan3::create_samplers(*model, args, init_contexts, 
-//                                         metric_contexts, *logger, init_writers);
-    
-//     // Should hold diag_e config
-//     EXPECT_TRUE(std::holds_alternative<
-//       stan3::sampler_config<stan::mcmc::adapt_diag_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant));
-    
-//     auto& config = std::get<
-//       stan3::sampler_config<stan::mcmc::adapt_diag_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-//   });
-// }
+  for (size_t i = 0; i < args_.num_chains; ++i) {
+    EXPECT_EQ(config.init_params[i].size(), model_->num_params_r());
+  }
+}
 
-// TEST_F(LoadSamplersTest, CreateSamplersVariantDenseE) {
-//   args.metric_type = stan3::metric_t::DENSE_E;
-//   args.num_chains = 1;
+TEST_F(LoadSamplersTest, LoadSamplers_SingleChain) {
+  args_.num_chains = 1;
   
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
+  // Resize contexts for single chain
+  init_contexts_.resize(1);
+  metric_contexts_.resize(1);
+  init_writers_.resize(1);
   
-//   EXPECT_NO_THROW({
-//     auto variant = stan3::create_samplers(*model, args, init_contexts, 
-//                                         metric_contexts, *logger, init_writers);
-    
-//     // Should hold dense_e config
-//     EXPECT_TRUE(std::holds_alternative<
-//       stan3::sampler_config<stan::mcmc::adapt_dense_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant));
-    
-//     auto& config = std::get<
-//       stan3::sampler_config<stan::mcmc::adapt_dense_e_nuts<stan::model::model_base, stan3::rng_t>>
-//     >(variant);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-//   });
-// }
+  auto config = stan3::load_samplers<stan3::metric_t::DIAG_E>(
+    *model_, args_, init_contexts_, metric_contexts_, *logger_, init_writers_);
+  
+  EXPECT_EQ(config.samplers.size(), 1);
+  EXPECT_EQ(config.rngs.size(), 1);
+  EXPECT_EQ(config.init_params.size(), 1);
+  EXPECT_EQ(config.init_params[0].size(), model_->num_params_r());
+}
 
-// TEST_F(LoadSamplersTest, LoadSamplersWithEmptyInitContext) {
-//   args.metric_type = stan3::metric_t::UNIT_E;
-//   args.num_chains = 1;
+TEST_F(LoadSamplersTest, SamplerTraits_TypeAliases) {
+  // Test that sampler_traits compile correctly
+  using diag_sampler = stan3::sampler_traits<stan3::metric_t::DIAG_E>::sampler_type<bernoulli_model_namespace::bernoulli_model>;
+  using unit_sampler = stan3::sampler_traits<stan3::metric_t::UNIT_E>::sampler_type<bernoulli_model_namespace::bernoulli_model>;
+  using dense_sampler = stan3::sampler_traits<stan3::metric_t::DENSE_E>::sampler_type<bernoulli_model_namespace::bernoulli_model>;
   
-//   auto init_contexts = create_init_contexts(args.num_chains, false);  // use empty context
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
-  
-//   EXPECT_NO_THROW({
-//     auto config = stan3::load_samplers<stan3::metric_t::UNIT_E>(
-//       *model, args, init_contexts, metric_contexts, *logger, init_writers);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-//     EXPECT_FALSE(config.init_params[0].empty());
-//   });
-// }
-
-// TEST_F(LoadSamplersTest, LoadSamplersWithNullInitWriter) {
-//   args.metric_type = stan3::metric_t::UNIT_E;
-//   args.num_chains = 1;
-  
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-  
-//   // Create init writers with null pointer
-//   std::vector<stan::callbacks::writer*> init_writers = {nullptr};
-  
-//   EXPECT_NO_THROW({
-//     auto config = stan3::load_samplers<stan3::metric_t::UNIT_E>(
-//       *model, args, init_contexts, metric_contexts, *logger, init_writers);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-//     EXPECT_FALSE(config.init_params[0].empty());
-//   });
-// }
-
-// TEST_F(LoadSamplersTest, SamplerConfigurationParameters) {
-//   args.metric_type = stan3::metric_t::DIAG_E;
-//   args.num_chains = 1;
-//   args.stepsize = 1.5;
-//   args.stepsize_jitter = 0.1;
-//   args.max_depth = 12;
-//   args.delta = 0.85;
-  
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
-  
-//   EXPECT_NO_THROW({
-//     auto config = stan3::load_samplers<stan3::metric_t::DIAG_E>(
-//       *model, args, init_contexts, metric_contexts, *logger, init_writers);
-    
-//     EXPECT_EQ(config.samplers.size(), 1);
-    
-//     // Verify sampler is configured with correct parameters
-//     auto& sampler = config.samplers[0];
-    
-//     // These are harder to test directly, but we can at least verify
-//     // the sampler was created and configured without throwing
-//     EXPECT_NO_THROW({
-//       // Try to access adaptation parameters
-//       auto& adaptation = sampler.get_stepsize_adaptation();
-//       // If we get here, the sampler was properly configured
-//     });
-//   });
-// }
-
-// TEST_F(LoadSamplersTest, ErrorHandlingInvalidMetricType) {
-//   // Test with an invalid metric type cast
-//   args.metric_type = static_cast<stan3::metric_t>(999);
-//   args.num_chains = 1;
-  
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, stan3::metric_t::UNIT_E);
-//   auto init_writers = create_init_writers(args.num_chains);
-  
-//   EXPECT_THROW({
-//     auto variant = stan3::create_samplers(*model, args, init_contexts, 
-//                                         metric_contexts, *logger, init_writers);
-//   }, std::runtime_error);
-// }
-
-// TEST_F(LoadSamplersTest, ConsistentRNGSeeds) {
-//   args.metric_type = stan3::metric_t::UNIT_E;
-//   args.num_chains = 3;
-//   args.random_seed = 42;
-  
-//   auto init_contexts = create_init_contexts(args.num_chains);
-//   auto metric_contexts = create_metric_contexts(args.num_chains, args.metric_type);
-//   auto init_writers = create_init_writers(args.num_chains);
-  
-//   // Create samplers twice with same seed
-//   auto config1 = stan3::load_samplers<stan3::metric_t::UNIT_E>(
-//     *model, args, init_contexts, metric_contexts, *logger, init_writers);
-  
-//   auto config2 = stan3::load_samplers<stan3::metric_t::UNIT_E>(
-//     *model, args, init_contexts, metric_contexts, *logger, init_writers);
-  
-//   EXPECT_EQ(config1.samplers.size(), config2.samplers.size());
-//   EXPECT_EQ(config1.rngs.size(), config2.rngs.size());
-  
-//   // Initial parameters should be the same with same seed and init context
-//   for (size_t i = 0; i < config1.init_params.size(); ++i) {
-//     EXPECT_EQ(config1.init_params[i].size(), config2.init_params[i].size());
-//   }
-// }
-
+  // Just check that these types compile - we can't easily instantiate them here
+  EXPECT_TRUE(true);  // Placeholder assertion
+}
---
+++ src/test/unit/run_samplers_test.cpp
@@ -0,0 +1,172 @@
+#include <stan3/run_samplers.hpp>
+#include <stan3/hmc_nuts_arguments.hpp>
+#include <stan3/hmc_output_writers.hpp>
+#include <stan3/read_json_data.hpp>
+
+#include <test/test-models/bernoulli.hpp>
+
+#include <stan/callbacks/stream_logger.hpp>
+#include <stan/callbacks/interrupt.hpp>
+
+#include <filesystem>
+#include <memory>
+#include <vector>
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+class RunSamplersTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    // Create temporary output directory
+    temp_dir_ = std::filesystem::temp_directory_path() / "run_samplers_test";
+    std::filesystem::create_directories(temp_dir_);
+    
+    // Load test data and create model
+    auto data_context = stan3::read_json_data("src/test/test-models/bernoulli.data.json");
+    model_ = std::make_unique<bernoulli_model_namespace::bernoulli_model>(*data_context, 12345);
+    
+    // Setup minimal arguments for fast testing
+    args_.num_chains = 1;
+    args_.random_seed = 12345;
+    args_.metric_type = stan3::metric_t::UNIT_E;  // Fastest option
+    args_.num_warmup = 10;    // Very small for testing
+    args_.num_samples = 10;   // Very small for testing
+    args_.stepsize = 1.0;
+    args_.max_depth = 5;      // Small for speed
+    args_.delta = 0.8;
+    args_.refresh = 0;        // No progress messages
+    args_.output_dir = temp_dir_.string();
+    
+    // Create contexts
+    for (size_t i = 0; i < args_.num_chains; ++i) {
+      init_contexts_.push_back(stan3::read_json_data(""));
+      metric_contexts_.push_back(stan3::read_json_data(""));
+    }
+    
+    // Create writers
+    writers_ = stan3::create_hmc_nuts_multi_chain_writers(args_, "test_model");
+    
+    // Create logger and interrupt
+    logger_ = std::make_unique<stan::callbacks::stream_logger>(
+      log_stream_, log_stream_, log_stream_, log_stream_, log_stream_);
+    interrupt_ = std::make_unique<stan::callbacks::interrupt>();
+  }
+  
+  void TearDown() override {
+    std::filesystem::remove_all(temp_dir_);
+  }
+  
+  std::filesystem::path temp_dir_;
+  std::unique_ptr<bernoulli_model_namespace::bernoulli_model> model_;
+  stan3::hmc_nuts_args args_;
+  std::vector<std::shared_ptr<const stan::io::var_context>> init_contexts_;
+  std::vector<std::shared_ptr<const stan::io::var_context>> metric_contexts_;
+  std::vector<stan3::hmc_nuts_writers> writers_;
+  std::unique_ptr<stan::callbacks::stream_logger> logger_;
+  std::unique_ptr<stan::callbacks::interrupt> interrupt_;
+  std::stringstream log_stream_;  // Capture log output
+};
+
+TEST_F(RunSamplersTest, RunSamplers_SingleChain_UnitE) {
+  args_.num_chains = 1;
+  args_.metric_type = stan3::metric_t::UNIT_E;
+  
+  // Resize contexts and writers for single chain
+  init_contexts_.resize(1);
+  metric_contexts_.resize(1);
+  writers_.resize(1);
+  
+  EXPECT_NO_THROW({
+    stan3::run_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                       writers_, *interrupt_, *logger_);
+  });
+  
+  // Check that sample file was created
+  EXPECT_TRUE(std::filesystem::exists(temp_dir_));
+}
+
+TEST_F(RunSamplersTest, RunSamplers_SingleChain_DiagE) {
+  args_.num_chains = 1;
+  args_.metric_type = stan3::metric_t::DIAG_E;
+  
+  init_contexts_.resize(1);
+  metric_contexts_.resize(1);
+  writers_.resize(1);
+  
+  EXPECT_NO_THROW({
+    stan3::run_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                       writers_, *interrupt_, *logger_);
+  });
+}
+
+TEST_F(RunSamplersTest, RunSamplers_MultipleChains) {
+  args_.num_chains = 2;
+  args_.metric_type = stan3::metric_t::UNIT_E;
+  
+  // Resize contexts and writers for multiple chains
+  init_contexts_.resize(2);
+  metric_contexts_.resize(2);
+  writers_.resize(2);
+  
+  // Add second chain contexts
+  init_contexts_[1] = stan3::read_json_data("");
+  metric_contexts_[1] = stan3::read_json_data("");
+  
+  // Create second chain writers
+  writers_[1] = stan3::create_hmc_nuts_single_chain_writers(
+    args_, "test_model", stan3::generate_timestamp(), 2);
+  
+  EXPECT_NO_THROW({
+    stan3::run_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                       writers_, *interrupt_, *logger_);
+  });
+}
+
+TEST_F(RunSamplersTest, SamplerRunner_Construction) {
+  stan3::sampler_runner runner(*model_, args_, writers_, *interrupt_, *logger_);
+  
+  // Just test that construction doesn't throw
+  EXPECT_TRUE(true);
+}
+
+TEST_F(RunSamplersTest, RunSamplers_MinimalSampling) {
+  // Test with absolute minimal sampling to ensure basic functionality
+  args_.num_chains = 1;
+  args_.num_warmup = 1;
+  args_.num_samples = 1;
+  args_.metric_type = stan3::metric_t::UNIT_E;
+  
+  init_contexts_.resize(1);
+  metric_contexts_.resize(1);
+  writers_.resize(1);
+  
+  EXPECT_NO_THROW({
+    stan3::run_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                       writers_, *interrupt_, *logger_);
+  });
+}
+
+TEST_F(RunSamplersTest, RunSamplers_WithNullableWriters) {
+  args_.num_chains = 1;
+  args_.save_start_params = false;  // This should make start_params_writer null
+  args_.save_diagnostics = false;   // This should make diagnostics_writer null
+  args_.save_metric = false;        // This should make metric_writer null
+  
+  // Recreate writers with nullable options
+  writers_ = stan3::create_hmc_nuts_multi_chain_writers(args_, "test_model");
+  
+  init_contexts_.resize(1);
+  metric_contexts_.resize(1);
+  
+  EXPECT_NO_THROW({
+    stan3::run_samplers(*model_, args_, init_contexts_, metric_contexts_, 
+                       writers_, *interrupt_, *logger_);
+  });
+  
+  // Verify that nullable writers are actually null
+  EXPECT_EQ(writers_[0].start_params_writer, nullptr);
+  EXPECT_EQ(writers_[0].diagnostics_writer, nullptr);
+  EXPECT_EQ(writers_[0].metric_writer, nullptr);
+  EXPECT_NE(writers_[0].sample_writer, nullptr);  // Sample writer should never be null
+}
